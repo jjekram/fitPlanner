604988e5f7ec94e2015b94076fa5109b2bb6c9d0
Jal Jalali Ekram <jjekram@gmail.com>
2013-10-24 20:40:57 -0400

    Setting up fitPlanner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitPlanner::Application.config.secret_token = '60c35f2141022d6a2eb5d439e82539788507dac21735cbc2cae53277f9e89193b02a37d221497844dc57a7ac527377ba032ec6d17b00fb772dce4bc157d29233'
